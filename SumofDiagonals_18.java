adfc5d94fd1c0df5c00b3f893097b89be75fa291
RahaviS <108854458+RahaviS@users.noreply.github.com>
2022-08-30 18:05:33 +0530

    Add files via upload

---
+++ SumofDiagonals_18.java
@@ -0,0 +1,21 @@
+package com.zsgs2;
+
+public class SumofDiagonals_18 {
+    public static void main(String[] args) {
+        int[][] arr={{1,2,3},
+                     {4,5,6},
+                     {7,8,9}};
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length;j++){
+                if(i==j) {
+                    sum+=arr[i][j];
+                }
+                if((i+j+1)==arr.length){
+                    sum+=arr[i][j];
+                }
+            }
+        }
+        System.out.print("Sum of Diagonals "+sum);
+    }
+}
